9ea01d8cf1a4c8bc6fc139f1e632e5cae1998fb9
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2024-11-18 10:38:02 +0700

    Remove .env file from tracking

--- vietmap_config.js
+++ vietmap_config.js
@@ -1,4 +1,4 @@
-export const vietmapAPIKey = "7c3ab066a578a4f5fe4f40c67573f7e0a831f9b36adfdc48";
+export const vietmapAPIKey = "";
 
 export const vietmapStyle = `https://maps.vietmap.vn/api/maps/light/styles.json?apikey=${vietmapAPIKey}`;
 export const vietmapRasterStyle = `https://maps.vietmap.vn/api/maps/raster/styles.json?apikey=${vietmapAPIKey}`;
